3bf28cc59cd7aeba0d156b9c076f61bfb96d935e
mRcfps <1402491442@qq.com>
2018-06-04 18:21:43 +0800

    cli: Use prompts rather than promptly for better cli UX

--- utils/destroy.js
+++ utils/destroy.js
@@ -1,29 +1,30 @@
 const ora = require('ora');
-const promptly = require('promptly');
-const signale = require('signale');
+const prompts = require('prompts');
 
 const common = require('./common');
 
 module.exports = async (options) => {
   if (!common.ifTutureSuiteExists()) {
-    signale.error('No Tuture tutorial to destroy!');
+    console.log('No Tuture tutorial to destroy!');
     process.exit(1);
   }
-  try {
-    const answer = options.force ? true : await promptly.confirm(
-      'Are you sure? [y/N] ',
-      { default: 'n' },
-    );
-    if (!answer) {
-      console.log('Aborted!');
-      process.exit(1);
-    }
-    const spinner = ora('Deleting Tuture files...').start();
-    await common.removeTutureSuite();
-    spinner.stop();
-    signale.success('Tuture suite has been destroyed!');
-  } catch (e) {
-    console.log('\nAborted!');
+
+  const onCancel = () => {
+    console.log('Aborted!');
+    process.exit(1);
+  };
+
+  const answer = options.force ? true : await prompts({
+    type: 'confirm',
+    name: 'answer',
+    message: 'Are you sure?',
+    initial: false,
+  }, { onCancel });
+  if (!answer) {
+    console.log('Aborted!');
     process.exit(1);
   }
+  const spinner = ora('Deleting Tuture files...').start();
+  await common.removeTutureSuite();
+  spinner.succeed('Tuture suite has been destroyed!');
 };
