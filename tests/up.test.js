949fad415a7c8f53bf38b7c8fe79ebe48a096d1c
mRcfps <1402491442@qq.com>
2018-07-28 09:08:43 +0800

    Add test case of tuture.yml syntax error

--- tests/up.test.js
+++ tests/up.test.js
@@ -1,4 +1,6 @@
 const fs = require('fs-extra');
+const path = require('path');
+
 const utils = require('./utils');
 
 // Tmp directories used in tests.
@@ -18,4 +20,20 @@ describe('tuture up', () => {
       expect(cp.status).toBe(1);
     });
   });
+
+  describe('tuture.yml syntax error', () => {
+    const repoPath = utils.createGitRepo();
+    const tutureRunner = utils.tutureRunnerFactory(repoPath);
+    tmpDirs.push(repoPath);
+    tutureRunner(['init', '-y']);
+
+    const flawedTuture = 'foo: bar:';
+    fs.writeFileSync(path.join(repoPath, 'tuture.yml'), flawedTuture);
+
+    it('should report syntax error', () => {
+      const cp = tutureRunner(['up']);
+      expect(cp.status).toBe(1);
+      expect(cp.stdout.toString()).toMatch(/syntax error/);
+    });
+  })
 });
