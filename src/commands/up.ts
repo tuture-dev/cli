e2785f71a4ca78bf89fed60c2e687a100e76c6c1
mRcfps <1402491442@qq.com>
2018-08-29 15:28:05 +0800

    Add port flags for up command

--- src/commands/up.ts
+++ src/commands/up.ts
@@ -1,8 +1,9 @@
 import fs from 'fs-extra';
 import which from 'which';
-import spawn from 'cross-spawn';
 import yaml from 'js-yaml';
+import spawn from 'cross-spawn';
 import { prompt } from 'inquirer';
+import { flags } from '@oclif/command';
 
 import BaseCommand from '../base';
 import reload from './reload';
@@ -14,6 +15,14 @@ type ConfirmResponse = {
 export default class Up extends BaseCommand {
   static description = 'Render and edit tutorial in browser';
 
+  static flags = {
+    help: flags.help({ char: 'h' }),
+    port: flags.integer({
+      char: 'p',
+      description: 'which port to use for tutorial server',
+    }),
+  };
+
   spawnServerProcess(port: number) {
     return spawn.sync(
       'tuture-server',
@@ -23,18 +32,20 @@ export default class Up extends BaseCommand {
     );
   }
 
-  async fireTutureServer() {
-    let port = 3000;
-    let serverProc = this.spawnServerProcess(port);
+  async fireTutureServer(port?: number) {
+    let portToUse = port || 3000;
+    let serverProc = this.spawnServerProcess(portToUse);
 
     // Port 3000 is occupied.
     if (serverProc.status !== 0) {
-      const response = await prompt([{
-        type: 'confirm',
-        name: 'answer',
-        message: `Port ${port} has been used. Do you want to use a new port?`,
-        default: true,
-      }]);
+      const response = await prompt([
+        {
+          type: 'confirm',
+          name: 'answer',
+          message: `Port ${portToUse} has been used. Do you want to use a new port?`,
+          default: true,
+        },
+      ]);
 
       if (!(response as ConfirmResponse).answer) {
         this.exit(0);
@@ -43,13 +54,13 @@ export default class Up extends BaseCommand {
 
     // Incrementing port number until it's available.
     while (serverProc.status !== 0) {
-      port += 1;
-      serverProc = this.spawnServerProcess(port);
+      portToUse += 1;
+      serverProc = this.spawnServerProcess(portToUse);
     }
   }
 
   async run() {
-    this.parse(Up);
+    const { flags } = this.parse(Up);
 
     if (!fs.existsSync('tuture.yml')) {
       this.error('tuture.yml not found!');
@@ -71,6 +82,6 @@ export default class Up extends BaseCommand {
 
     await reload.run([]);
 
-    this.fireTutureServer();
+    this.fireTutureServer(flags.port);
   }
 }
